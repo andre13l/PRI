19a7d867bf084a6139c3694cd06e03b996c9d953
Guilherme Diogo <up201806340@fe.up.pt>
2023-10-06 10:15:18 +0100

    scraping

--- scraper.py
+++ scraper.py
@@ -1,13 +1,28 @@
 import requests
-
 from bs4 import BeautifulSoup
 
 def fetch_reviews():
-    url = 'https://www.yelp.com/biz/la-crosta-woodfire-pizzeria-los-angeles-2?osq=La+Crosta+Woodfire+Pizzeria'
+    url = 'https://www.yelp.pt/biz/le-grand-los-angeles-3'
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    reviews = soup.find_all('div', {'class': 'review-content'})
-    for review in reviews:
-        print(review.find('p').text)
 
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        
+        # Check if the reviews container is present
+        reviews = soup.find_all('div', {'class': 'review-content'})
+        if reviews:
+            for review in reviews:
+                # Check if the review text is present
+                review_text = review.find('p')
+                if review_text:
+                    print(review_text.text)
+                else:
+                    print("No review text found in a review.")
+        else:
+            print("No reviews found on the page.")
+    else:
+        print(f"Failed to retrieve the page. Status code: {response.status_code}")
+
+# Call the function
 fetch_reviews()
